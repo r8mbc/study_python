ee3aa7b7677f70b5f4af264deb7708d0d09f056b
r8ckr <tfstnt.tyby888@gmail.com>
2022-03-16 00:07:29 +0900

    03/15 お試し問題を解いた

---
+++ AtCorder/AtCoder Beginners Selection/shift_only.py
@@ -0,0 +1,13 @@
+n = input()
+a = list(map(int, input().split()))
+
+for i in range(len(a)):
+    count = 0
+    while a[i] % 2 == 0:
+        a[i] = a[i] // 2
+        count += 1
+    a[i] = count
+
+a.sort()
+
+print(a[0])
