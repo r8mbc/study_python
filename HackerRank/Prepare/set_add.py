747149c9006b28930c640edce5bcec5bbb3d9c61
r8ckr <tfstnt.tyby888@gmail.com>
2022-03-22 14:14:13 +0900

    addを使う

---
+++ HackerRank/Prepare/set_add.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/py-set-add/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+n = int(input())
+c = set()
+
+for i in range(n):
+    c.add(input())
+
+print(len(c))
