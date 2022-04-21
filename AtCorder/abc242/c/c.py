f34133fd15dde30f62b551e82acfa68ec81a12c6
r8ckr <tfstnt.tyby888@gmail.com>
2022-04-21 20:13:40 +0900

    ABC242 C問題正解：早くするのにすごく苦労した

--- AtCorder/abc242/c/c.py
+++ AtCorder/abc242/c/c.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
-n = int(input())
+import sys
 
-ans = 9
-m = 2
+input = sys.stdin.readline
 
-for i in range(n - 1):
-    ans *= 3
 
-for i in range(n - 1):
-    m *= 3
+def func():
+    n = int(input())
+    s = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    mod = 998244353
 
-print(ans, m)
+    for _ in range(1, n):
+        m = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        m[0] = s[0] + s[1] % mod
+        m[1] = s[0] + s[1] + s[2] % mod
+        m[2] = s[1] + s[2] + s[3] % mod
+        m[3] = s[2] + s[3] + s[4] % mod
+        m[4] = s[3] + s[4] + s[5] % mod
+        m[5] = s[4] + s[5] + s[6] % mod
+        m[6] = s[5] + s[6] + s[7] % mod
+        m[7] = s[6] + s[7] + s[8] % mod
+        m[8] = s[7] + s[8] % mod
+        s = m
+
+    return sum(s) % mod
+
+
+print(func())
