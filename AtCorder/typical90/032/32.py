1d97558df93d6acf4495bb71c8e3a2e6180064ba
r8ckr <tfstnt.tyby888@gmail.com>
2022-05-31 15:40:43 +0900

    「多次元配列の初期化はfor」を学んだ

--- AtCorder/typical90/032/32.py
+++ AtCorder/typical90/032/32.py
@@ -4,17 +4,36 @@ from itertools import permutations
 
 n = int(input())
 
-A = [input().split() for _ in range(n)]
+A = [list(map(int, input().split())) for _ in range(n)]
 
 m = int(input())
 
-xy = [input().split() for _ in range(m)]
+xy = [[True] * n for _ in range(n)]
 
-runner = [i for i in range(1, n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    xy[x - 1][y - 1] = False
+    xy[y - 1][x - 1] = False
+
+
+runner = [i for i in range(n)]
 ans = pow(10, 1000)
 
 for sosha in permutations(runner):
-    kyori = 0
-    for i in sosha:
-        for j in range(n):
-            kyori += A[i][j]
+
+    bool = True
+    for i in range(n - 1):
+        if xy[sosha[i]][sosha[i + 1]] == False:
+            bool = False
+            break
+
+    if bool:
+        kyori = 0
+        for i in range(n):
+            kyori += A[sosha[i]][i]
+        ans = min(kyori, ans)
+
+if ans == pow(10, 1000):
+    print(-1)
+else:
+    print(ans)
