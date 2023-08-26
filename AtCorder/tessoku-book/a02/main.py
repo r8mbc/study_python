e8579b4712ce152b6cbf11b86b621ea9038e5ba9
r8ckr <tfstnt.tyby888@gmail.com>
2023-08-26 19:12:38 +0900

    20230826

--- AtCorder/tessoku-book/a02/main.py
+++ AtCorder/tessoku-book/a02/main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 
+n, x = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+if x in A:
+    print("Yes")
+else:
+    print("No")
--- AtCorder/tessoku-book/b04/main.py
+++ AtCorder/tessoku-book/b04/main.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 
+N = str(input())
+
+ans = 0
+X = []
+for i in range(len(N)):
+    X.insert(0, 2**i)
+
+for i in range(len(N)):
+    ans += X[i] * int(N[i])
+
+print(ans)
