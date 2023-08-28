9fd6dedc1f4c3c1d72ca80638d093ff7e0e3be4a
r8ckr <tfstnt.tyby888@gmail.com>
2023-08-28 18:50:42 +0900

    累積和（応用問題）

--- AtCorder/tessoku-book/b06/main.py
+++ AtCorder/tessoku-book/b06/main.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
 
+n = int(input())
+
+A = list(map(int, input().split()))
+
+sum_A = [0]
+now = 0
+for i in range(n):
+    now += A[i]
+    sum_A.append(now)
+
+q = int(input())
+
+for _ in range(q):
+    l, r = map(int, input().split())
+
+    # あたり
+    c = sum_A[r] - sum_A[l - 1]
+    # はずれ
+    b = r - l + 1 - c
+
+    if b > c:
+        print("lose")
+    elif c > b:
+        print("win")
+    else:
+        print("draw")
