06cbb2eb5a62ef077533b772ddd942888f2cc1fb
r8ckr <tfstnt.tyby888@gmail.com>
2023-08-29 16:56:01 +0900

    累積和2、いもす法

--- AtCorder/tessoku-book/a07/main.py
+++ AtCorder/tessoku-book/a07/main.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 
+d = int(input())
+n = int(input())
+
+Z = [0] * d
+
+for _ in range(n):
+    l, r = map(int, input().split())
+    Z[l - 1] += 1
+    if r == d:
+        continue
+    Z[r] -= 1
+
+ans = 0
+
+for i in Z:
+    ans += i
+    print(ans)
