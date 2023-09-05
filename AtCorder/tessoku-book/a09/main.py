7d1fa2c286a6def11cf2448bd069ad566d899842
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-05 19:21:43 +0900

    二次元の累積和（２）

--- AtCorder/tessoku-book/a09/main.py
+++ AtCorder/tessoku-book/a09/main.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3
 
+h, w, n = map(int, input().split())
+
+X = [([0] * (w + 2)) for _ in range(h + 2)]
+
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    X[a][b] += 1
+    X[a][d + 1] -= 1
+    X[c + 1][b] -= 1
+    X[c + 1][d + 1] += 1
+
+for i in range(h + 2):
+    now = 0
+    for j in range(w + 2):
+        now += X[i][j]
+        X[i][j] = now
+
+for i in range(w + 2):
+    now = 0
+    for j in range(h + 2):
+        now += X[j][i]
+        X[j][i] = now
+
+for i in range(1, h + 1):
+    print(*X[i][1:-1])
