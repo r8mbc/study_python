edefb4e053bad4507e33cfac5498e1e348a533da
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-08 18:30:44 +0900

    配列の二分探索（応用問題）

--- AtCorder/tessoku-book/b11/main.py
+++ AtCorder/tessoku-book/b11/main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 
+import bisect
+
+n = int(input())
+A = list(map(int, input().split()))
+sortA = sorted(A)
+q = int(input())
+
+for _ in range(q):
+    x = int(input())
+
+    ans = bisect.bisect_left(sortA,x)
+    print(ans)
