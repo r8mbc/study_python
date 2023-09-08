c31fe41786ae6a7ed19c0ed3aea701f04b7c2ca8
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-08 18:20:34 +0900

    配列の二分探索

--- AtCorder/tessoku-book/a11/main.py
+++ AtCorder/tessoku-book/a11/main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+n, x = map(int, input().split())
+A = list(map(int, input().split()))
+
+l = 0
+r = n
+
+for _ in range(n):
+    m = (l + r) // 2
+    if x == A[m]:
+        print(m + 1)
+        exit()
+    elif x < A[m]:
+        r = m
+        continue
+    elif x > A[m]:
+        l = m
