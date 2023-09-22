90ce6a71571117da4a85d666a861d1ccb289e119
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-22 22:54:33 +0900

    しゃくとり法。概念の理解が少し難しかった

--- AtCorder/tessoku-book/a13/main.py
+++ AtCorder/tessoku-book/a13/main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 
+n, k = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = [0] * n
+
+ans = 0
+
+for i in range(n - 1):
+
+    if i != 0:
+        B[i] = B[i - 1]
+
+    while (B[i] + 1 < n) and (A[B[i] + 1] - A[i] <= k):
+        B[i] += 1
+
+    ans += B[i] - i
+
+print(ans)
