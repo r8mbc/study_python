f0bc52cb3b97aa06ea9980e163b9c0dd566889f7
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-26 00:18:19 +0900

    動的計画法基礎

--- AtCorder/tessoku-book/a16/main.py
+++ AtCorder/tessoku-book/a16/main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+n = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+dp = [0] * n
+
+for i in range(n):
+    if i == 0:
+        continue
+    if i == 1:
+        dp[i] = A[0]
+    else:
+        dp[i] = min(dp[i - 1] + A[i - 1], dp[i - 2] + B[i - 2])
+
+print(dp[-1])
