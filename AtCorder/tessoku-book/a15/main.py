f8a430644681289f5d38359f327308b40e2b4631
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-25 19:22:26 +0900

    チャレンジ問題、二分探索

--- AtCorder/tessoku-book/a15/main.py
+++ AtCorder/tessoku-book/a15/main.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+import bisect
+
+n = int(input())
+
+A = list(map(int, input().split()))
+
+sA = sorted(set(A))
+
+ans = []
+for i in range(n):
+    bserach = bisect.bisect_left(sA, A[i])
+    ans.append(bserach + 1)
+
+print(*ans)
