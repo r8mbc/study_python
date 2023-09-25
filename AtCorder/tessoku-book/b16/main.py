e146d8f97edb1a7e7c3db25280dac9c849b3d62f
r8ckr <tfstnt.tyby888@gmail.com>
2023-09-26 00:25:12 +0900

    動的計画法基礎（応用問題）

--- AtCorder/tessoku-book/b16/main.py
+++ AtCorder/tessoku-book/b16/main.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+n = int(input())
+H = list(map(int, input().split()))
+
+dp = [0] * n
+
+for i in range(n):
+    if i == 0:
+        continue
+    elif i == 1:
+        dp[i] = abs(H[i] - H[0])
+    else:
+        dp[i] = min(dp[i - 1] + abs(H[i - 1] - H[i]), dp[i - 2] + abs(H[i - 2] - H[i]))
+
+print(dp[-1])
